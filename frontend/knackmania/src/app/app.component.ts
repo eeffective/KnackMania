02d88dfc7f6cb0621d67b9814ca929b379396d6a
Efaliso <efalisotamerat@gmail.com>
2020-06-12 16:28:51 +0200

    frontend added

---
+++ frontend/knackmania/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'knackmania';
+
+  update: boolean = false;
+  
+
+  constructor(updates: SwUpdate){
+    updates.available.subscribe(event => {
+
+      this.update = true;
+    })
+  }
+}
